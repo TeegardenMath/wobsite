02ff34ac66c80b7ac99cfc58db2aa2f56cdc0619
SDever <72769325+TeegardenMath@users.noreply.github.com>
2024-02-20 15:05:23 -0800

    prettier submission success page

--- app/routes.py
+++ app/routes.py
@@ -18,10 +18,12 @@ def grade(scantron):
 		answerKey = f.read().splitlines()
 
 	i=0
-	score=0
+	score=[]
 	while i < len(scantron):
 		if scantron[i] == float(answerKey[i]):
-			score+=1
+			score.append(1)
+		else:
+			score.append(0)
 		i+=1
 
 	return score
@@ -63,10 +65,18 @@ def main():
 				scantron.append(answer.answer.data)
 				problemcounter+=1
 
-			score = grade(scantron)
+			#this gives us a list of 0s and 1s
+			results = grade(scantron)
+
+			#add them up for total score
+			score=sum(results)
+
+			#concatenate them into a string to pass in url
+			results = "".join(str(x) for x in results)
+
+			#format them for postgres queries
 			problemkeylist=problemkeylist+",grade"
 			problemkeylist2=problemkeylist2+","+str(score)
-
 
 			#make a connection
 			with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
@@ -78,7 +88,7 @@ def main():
 					curs.execute(sqlstatement, answerlist)
 
 					#redirect on submission success
-					return redirect(url_for(".submitted",score=score,maxscore=problemcounter-1))
+					return redirect(url_for(".submitted",score=score,maxscore=problemcounter-1,results=results))
 	else:
 		print(form.errors)
 	return render_template("main.html", form=form)
@@ -104,9 +114,10 @@ def highscores():
 def submitted():
 	score=int(request.args.get("score"))
 	maxscore=int(request.args.get("maxscore"))
+	results=list(request.args.get("results"))
 	roundedperten=math.floor(10*score/maxscore)
 	congratslist=["Oof","Oof","Oof","Oof","Better luck next time","Better luck next time","Not so bad","Not too shabby","Nicely done","Awesome job","Fantabulous"]
-	return render_template("submitted.html",score=score,maxscore=maxscore,congrats=congratslist[roundedperten])
+	return render_template("submitted.html",score=score,maxscore=maxscore,congrats=congratslist[roundedperten],results=results)
 
 
 
