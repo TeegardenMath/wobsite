4f84b8ba716ed6ef963a4b971b0899ab4c774dec
SDever <72769325+TeegardenMath@users.noreply.github.com>
2024-02-21 16:02:07 -0800

    migrate tests to database

--- app/routes.py
+++ app/routes.py
@@ -13,14 +13,32 @@ CONNECTION_PARAMETERS = {
     "host": os.environ.get("DB_HOST"),
 }
 
-def grade(scantron):
-	with open('app/problembank/answerlist1.txt') as f:
-		answerKey = f.read().splitlines()
+def grade(scantron,testID):
+	#make a connection
+	with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+		#create a cursor
+		with conn.cursor() as curs:
+			#first we find the list of problem IDs for this test
+			curs.execute("""
+				SELECT problem_id
+				FROM problemtest
+				WHERE test_id=%s;
+				""",[testID])
+			problemIDs = curs.fetchall()
+
+			#now we find the problems for those IDs
+			curs.execute("""
+				SELECT answer, answertype, points
+				FROM problems
+				WHERE id = ANY(%s);
+				""", (problemIDs,))
+			#fetch the records
+			answerKey = curs.fetchall()
 
 	i=0
 	score=[]
 	while i < len(scantron):
-		if scantron[i] == float(answerKey[i]):
+		if scantron[i] == float(answerKey[i][0]):
 			score.append(1)
 		else:
 			score.append(0)
@@ -28,70 +46,116 @@ def grade(scantron):
 
 	return score
 
+def openTest(testID):
+	#make a connection
+	with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+		#create a cursor
+		with conn.cursor() as curs:
+			#first we find the list of problem IDs for this test
+			curs.execute("""
+				SELECT problem_id
+				FROM problemtest
+				WHERE test_id=%s;
+				""",[testID])
+			problemIDs = curs.fetchall()
 
+			#now we find the problems for those IDs
+			curs.execute("""
+				SELECT problem, answertype, unit, points
+				FROM problems
+				WHERE id = ANY(%s);
+				""", (problemIDs,))
+			#fetch the records
+			problemList = curs.fetchall()
 
-@bp.route("/", methods=['GET', 'POST'])
-def main():
+	#set up the input form
+	form = create_test_form(len(problemList))
 
-	#take the problem statements from external file
-	with open('app/problembank/problemlist1.txt') as f:
-		problemStatements = f.read().splitlines()
-
-	form = create_test_form(len(problemStatements))
-
-	#label the problems
+	#write the problems on the input form
 	ii = 0
 	for field in form.answers:
-		field.label=problemStatements[ii]
+		field.label=problemList[ii][0]
 		ii+=1
 
+	return form;
+	
+
+def gradeTest(form,testname):
+	#compile a dictionary of all the answers
+	answerlist={}
+	scantron=[]
+	problemcounter=1
+	problemkeylist="username,email"
+	problemkeylist2="'"+form.username.data+"\',\'"+form.email.data+"'"
+	for answer in form.answers:
+		problemkey = "answer"+str(problemcounter)
+		answerlist[problemkey]=answer.answer.data
+		problemkeylist=problemkeylist+","
+		problemkeylist2=problemkeylist2+","
+		problemkeylist=problemkeylist+problemkey
+		problemkeylist2=problemkeylist2+"%("+problemkey+")s"
+		scantron.append(answer.answer.data)
+		problemcounter+=1
+
+	#this gives us a list of 0s and 1s
+	results = grade(scantron,testname)
+
+	#add them up for total score
+	score=sum(results)
+
+	#concatenate them into a string to pass in url
+	results = "".join(str(x) for x in results)
+
+	#format them for postgres queries
+	problemkeylist=problemkeylist+",grade"
+	problemkeylist2=problemkeylist2+","+str(score)
+
+	#make a connection
+	with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+		#create a cursor
+		with conn.cursor() as curs:
+
+			#and now run that sql statement
+			sqlstatement="INSERT INTO submissions ("+problemkeylist+") VALUES ("+problemkeylist2+");"
+			curs.execute(sqlstatement, answerlist)
+
+			#redirect on submission success
+			return url_for(".submitted",score=score,maxscore=problemcounter-1,results=results)
+
+def extractTestList():
+	rawlist = os.listdir("app/problembank/problemlists")
+	processedList = []
+	for entry in rawlist:
+		splitEntry = entry.split(".")
+		processedList.append(splitEntry[0])
+	return processedList
+
+@bp.route("/")
+def main():
+	with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+		#create a cursor
+		with conn.cursor() as curs:
+			#first we find the list of problem IDs for this test
+			curs.execute("""
+				SELECT id, name
+				FROM tests
+				""")
+			testList = curs.fetchall()
+	return render_template("main.html", testlist=testList)
+
+
+@bp.route("/test/<testname>", methods=['GET', 'POST'])
+def test(testname):
+	form = openTest(testname)
 
 	#form submission stuff
 	if form.validate_on_submit():
-
-			#compile a dictionary of all the answers
-			answerlist={}
-			scantron=[]
-			problemcounter=1
-			problemkeylist="username,email"
-			problemkeylist2="'"+form.username.data+"\',\'"+form.email.data+"'"
-			for answer in form.answers:
-				problemkey = "answer"+str(problemcounter)
-				answerlist[problemkey]=answer.answer.data
-				problemkeylist=problemkeylist+","
-				problemkeylist2=problemkeylist2+","
-				problemkeylist=problemkeylist+problemkey
-				problemkeylist2=problemkeylist2+"%("+problemkey+")s"
-				scantron.append(answer.answer.data)
-				problemcounter+=1
-
-			#this gives us a list of 0s and 1s
-			results = grade(scantron)
-
-			#add them up for total score
-			score=sum(results)
-
-			#concatenate them into a string to pass in url
-			results = "".join(str(x) for x in results)
-
-			#format them for postgres queries
-			problemkeylist=problemkeylist+",grade"
-			problemkeylist2=problemkeylist2+","+str(score)
-
-			#make a connection
-			with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
-				#create a cursor
-				with conn.cursor() as curs:
-
-					#and now run that sql statement
-					sqlstatement="INSERT INTO submissions ("+problemkeylist+") VALUES ("+problemkeylist2+");"
-					curs.execute(sqlstatement, answerlist)
-
-					#redirect on submission success
-					return redirect(url_for(".submitted",score=score,maxscore=problemcounter-1,results=results))
+		urlreturn = gradeTest(form,testname)
+		return redirect(urlreturn)
 	else:
 		print(form.errors)
-	return render_template("main.html", form=form)
+
+	return render_template("test.html", form=form)
 
 
 @bp.route("/highscores")
