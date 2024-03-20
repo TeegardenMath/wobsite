39ca15aaff066821a0ba84b288b70b56952e5c17
SDever <72769325+TeegardenMath@users.noreply.github.com>
2024-03-20 16:51:57 -0700

    slightly fewer errors on tests

---
+++ tests/test_competitions.py
@@ -0,0 +1,26 @@
+import os
+import tempfile
+
+import pytest
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        with app.app_context():
+            app.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
+
+# def test_empty_db(client):
+#     """Start with a blank database."""
+
+#     rv = client.get('/')
+#     assert b'No entries here so far' in rv.data
